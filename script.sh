24a6f07f12b4d8111e28700150122a64aa038a26
faltimiras <faltimiras@newrelic.com>
2025-06-11 19:08:51 +0200

    get fleet form config

--- script.sh
+++ script.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-# Usage: ./script.sh <file_path>
+# Usage: ./script.sh <commit 1> <commit2>
 ENDPOINT_URL="http://ptsv3.com/t/alti-git-ops-test/post/"
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <file_path>"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <commit 1> <commit2>"
   exit 1
 fi
 
-FILE_PATH="$1"
 
-if [ ! -f "$FILE_PATH" ]; then
-  echo "File not found: $FILE_PATH"
-  exit 2
-fi
+git diff --name-only $1 $2 > changed_files.txt
 
-curl -X POST "$ENDPOINT_URL" --data-binary @"$FILE_PATH" -H "Content-Type: text/plain" -H "Api-Key:$API_KEY"
+
+YAML_FILE="./deployment_config.yml"
+TARGET_FILE=$(cat changed_files.txt)
+
+echo $TARGET_FILE
+export TARGET_FILE
+
+FLEET_GUID=$(yq -r '
+ .configurations[]
+  | select(.files[] == env(TARGET_FILE))
+  | .fleetGuid
+' "$YAML_FILE")
+
+echo $FLEET_GUID
+
+curl -X POST "$ENDPOINT_URL" --data-binary @"$TARGET_FILE" -H "Content-Type: text/plain" -H "Api-Key:$API_KEY" -H "fleetGuid:$FLEET_GUID"
+
+rm changed_files.txt
